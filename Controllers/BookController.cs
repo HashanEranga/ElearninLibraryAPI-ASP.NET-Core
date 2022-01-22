a9870be0ef5d1b309de1b6e7aba6ebbb57f89968
HashanEranga <45037796+ccmrhe@users.noreply.github.com>
2022-01-23 02:47:06 +0530

    feat(): Get All Books, Get selected book services

---
+++ Controllers/BookController.cs
@@ -0,0 +1,37 @@
+using ElearninLibraryAPI_ASP.Models;
+using ElearninLibraryAPI_ASP.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ElearninLibraryAPI_ASP.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+
+public class BookController : ControllerBase
+{
+    public BookController()
+    {
+
+    }
+
+    // GET All the Books
+    [HttpGet]
+    public ActionResult <List<Book>> GetAll()=> BookService.GetAll();
+
+    // GET a Book by Book ID
+    [HttpGet("{bookId}")]
+    public ActionResult <Book> Get(int bookId)
+    {
+        var book = BookService.Get(bookId);
+
+        if(book == null)
+            return NotFound();
+        
+        return book;
+    }
+
+    // POST a new Book item
+
+
+    // DELETE a existing Book item
+}
